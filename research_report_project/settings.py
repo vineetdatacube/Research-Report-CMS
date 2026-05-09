36aa9569a042ab5c00ccc5587e8466a37e281047
vineetdatacube <vineetkumar120987@gmail.com>
2026-05-09 16:09:01 +0530

    Fix ALLOWED_HOSTS for Render

--- research_report_project/settings.py
+++ research_report_project/settings.py
@@ -125,9 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
-ALLOWED_HOSTS = [
-    'research-report-cms.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['*']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
