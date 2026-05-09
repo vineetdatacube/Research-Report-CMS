48f2e96ed7e97224b279cd64d0e90f5333446612
vineetdatacube <vineetkumar120987@gmail.com>
2026-05-09 16:24:32 +0530

    Fix urls.py import error

--- research_report_project/urls.py
+++ research_report_project/urls.py
@@ -1,3 +1,10 @@
+from django.contrib import admin
+from django.urls import path
 from django.contrib.auth import views as auth_views
+from reports.views import dashboard
 
-path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+]
