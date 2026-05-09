f8434b1c50bba19257801d41dad272083206d5ac
vineetdatacube <vineetkumar120987@gmail.com>
2026-05-09 16:18:00 +0530

    Add login template

--- research_report_project/urls.py
+++ research_report_project/urls.py
@@ -1,13 +1,3 @@
-from django.contrib import admin
-from django.urls import path
 from django.contrib.auth import views as auth_views
-from reports import views   # ✅ correct import
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-
-    path('', views.dashboard, name='dashboard'),
-]
+path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
