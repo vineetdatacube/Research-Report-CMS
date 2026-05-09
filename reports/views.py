8e3f7e00620d035feab7a713afbff505b504b21b
vineetdatacube <vineetkumar120987@gmail.com>
2026-05-09 16:15:20 +0530

    Enable login protection

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
-    return HttpResponse("Dashboard is working")
+    return render(request, 'dashboard.html')
